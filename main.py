2bca129c667030b32855adc2256b55a3e08cef2a
AlexanderChernikov83 <Cahe4ka2@ya.ru>
2025-08-01 14:19:19 +0300

    add class hero
    создание класса hero  с атрибутами

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+class Hero:
+    def __init__(self, name, health=100, attack_power=20):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+    def attack(self, other):
+        damage = self.attack_power
+        other.health -= damage
+        print(f"{self.name} атакует {other.name} на {damage} урона!")
+
+    def is_alive(self):
+        return self.health > 0
