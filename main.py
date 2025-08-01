89dd31db9295e6f1853e05dff017b0ee69039289
AlexanderChernikov83 <Cahe4ka2@ya.ru>
2025-08-01 14:51:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,27 +14,38 @@ class Hero:
     def is_alive(self):
         return self.health > 0
 
-    class Game:
-        def __init__(self):
-            player_name = input("Введите имя вашего героя: ")
-            self.player = Hero(player_name)
-            self.computer = Hero("Компьютер")
 
-        def start(self):
-                round_num = 1
-                print("\nНачинается битва!")
-                while self.player.is_alive() and self.computer.is_alive():
-                    print(f"\n--- Раунд {round_num} ---")
+class Game:
+    def __init__(self):
+        player_name = input("Введите имя вашего героя: ")
+        self.player = Hero(player_name)
+        self.computer = Hero("Компьютер")
 
-                    # Ход игрока
-                    self.player.attack(self.computer)
-                    print(f"У {self.computer.name} осталось {self.computer.health} HP")
+    def start(self):
+        round_num = 1
+        print("\nНачинается битва!")
+        while self.player.is_alive() and self.computer.is_alive():
+            print(f"\n--- Раунд {round_num} ---")
 
-                    if not self.computer.is_alive():
-                        break
+            # Ход игрока
+            self.player.attack(self.computer)
+            print(f"У {self.computer.name} осталось {self.computer.health} HP")
 
-                    # Ход компьютера
-                    self.computer.attack(self.player)
-                    print(f"У {self.player.name} осталось {self.player.health} HP")
+            if not self.computer.is_alive():
+                break
 
-                    round_num += 1
+            # Ход компьютера
+            self.computer.attack(self.player)
+            print(f"У {self.player.name} осталось {self.player.health} HP")
+
+            round_num += 1
+
+        print("\nИгра окончена!")
+        if self.player.is_alive():
+            print(f"Победил {self.player.name}!")
+        else:
+            print("Победил Компьютер!")
+
+# Запуск игры
+game = Game()
+game.start()
