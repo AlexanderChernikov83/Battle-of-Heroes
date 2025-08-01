a54e92ee20623d025c9379e12c6bb8481b1cd90e
AlexanderChernikov83 <Cahe4ka2@ya.ru>
2025-08-01 14:40:43 +0300

    add method start
    реализация метода старт

--- main.py
+++ main.py
@@ -19,3 +19,22 @@ class Hero:
             player_name = input("Введите имя вашего героя: ")
             self.player = Hero(player_name)
             self.computer = Hero("Компьютер")
+
+        def start(self):
+                round_num = 1
+                print("\nНачинается битва!")
+                while self.player.is_alive() and self.computer.is_alive():
+                    print(f"\n--- Раунд {round_num} ---")
+
+                    # Ход игрока
+                    self.player.attack(self.computer)
+                    print(f"У {self.computer.name} осталось {self.computer.health} HP")
+
+                    if not self.computer.is_alive():
+                        break
+
+                    # Ход компьютера
+                    self.computer.attack(self.player)
+                    print(f"У {self.player.name} осталось {self.player.health} HP")
+
+                    round_num += 1
